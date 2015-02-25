1fa69c464de250a8e76d3524ab7d87107310b9f8
mycoin <nqliujiangtao@gmail.com>
2015-02-25 13:55:43 +0800

    update

--- app/js/startup.js
+++ app/js/startup.js
@@ -2,6 +2,17 @@
  * bootstraps angular onto the window.document node
  * NOTE: the ng-app attribute should not be on the index.html when using ng.bootstrap
  */
+if (!window.console || !console.firebug) {
+    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+        "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"
+    ];
+
+    window.console = {};
+    for (var i = 0; i < names.length; ++i) {
+        window.console[names[i]] = function() {}
+    }
+}
+
 define([
     'require',
     'angular',

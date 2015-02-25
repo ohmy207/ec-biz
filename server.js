99c4350440f7ed01f55c0e636a01d2305e802464
mycoin <nqliujiangtao@gmail.com>
2015-02-25 14:37:37 +0800

    update

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+var fs = require('fs');
+var path = require('path');
+
+if (!fs.existsSync('node_modules')) {
+
+    var ps = require('child_process').spawn(process.platform === "win32" ? "npm.cmd" : "npm", ['install'], {
+        stdio: 'inherit',
+        cwd: __dirname
+    });
+    ps.on('data', function(data) {
+        console.log(data);
+    });
+}
+
+if (!fs.existsSync('app/vendor')) {
+    var ps = require('child_process').spawn(process.platform === "win32" ? "bower.cmd" : "bower", ['install'], {
+        stdio: 'inherit',
+        cwd: __dirname
+    });
+    ps.on('data', function(data) {
+        console.log(data);
+    });
+}
+
 require('./lib/server');

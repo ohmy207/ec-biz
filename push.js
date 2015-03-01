41aeaba8ce50dbac1f0cf238f333d5e173cc94bd
mycoin <nqliujiangtao@gmail.com>
2015-03-02 01:08:08 +0800

    update

--- push.js
+++ push.js
@@ -4,6 +4,6 @@ var exec = require('child_process').exec;
 
 console.log('[git] commit and push.');
 exec("git add --all && git commit -m update && git push", function(error, stdout, stderr) {
-    console.log('[git] finish');
+    console.log(stdout);
 });
 // 
